b4a9765012d0038d1cfc3d9308bf97fa9a416429
BuildTools <unconfigured@null.spigotmc.org>
2022-06-13 13:03:57 -0400

    added server comments

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ const app = express();
 app.use(cors());
 
 app.post('/', upload.any(), async (req, res) => {
+    const time = new Date();
     const node1 = await getNodeOnePrice();
     const node2 = await getNodeTwoPrice();
     const nodeURL = node1 > node2 ? "https://node2.bundlr.network/price/1000" : "https://node1.bundlr.network/price/1000";
@@ -46,30 +47,33 @@ app.post('/', upload.any(), async (req, res) => {
     */
     const bundlr = new NodeBundlr.default(nodeURL, 'arweave', key);
 
-    /* Balance is given in Winston where 10^12 Winston = 1 AR. */
-    const balance = (await bundlr.getLoadedBalance()) / Math.pow(10,12);
     const file = req.files[0];
 
-    /* Tags are needed to specify how the file is uploaded on the Arweave permaweb */
-    const tags = [{ name: "Content-Type", value: file.mimetype}];
-    const tx = bundlr.createTransaction(file.buffer, {tags});
+    /* Balance is given in Winston where 10^12 Winston = 1 AR. */
+    const balance = (await bundlr.getLoadedBalance()) / Math.pow(10,12);
 
-    /* Price is similarly given in Winston */
-    const price = await bundlr.getPrice(tx.size) / Math.pow(10,12);
+    /* Price is similarly given in Winston. The transaction size is about 1000-2000 bytes larger than the file size */
+    const price = await bundlr.getPrice(file.size + 2000) / Math.pow(10,12);
     
     if (price > balance) {
         /* 
             The wallet can be funded with the async function bundlr.fund(). The amount funded should
-            be given in Winston as well.
+            be given in Winston as well. It can take up to an hour for the funds to actually get transferred. 
         */
+        const endTime = new Date()
+        res.json({message: `The file upload costs ${price} AR, but the wallet only has ${balance} AR available.`, time: time - endTime})
+    } else { 
 
-        res.json({message: `The file upload costs ${price} AR, but the wallet only has ${balance} AR available.`})
-    } else {
+        /* Tags are needed to specify how the file is uploaded on the Arweave permaweb */
+        const tags = [{ name: "Content-Type", value: file.mimetype}];
+        const tx = bundlr.createTransaction(file.buffer, {tags});
+
         await tx.sign();
         const id = tx.id;
         await tx.upload();
+
         /* The location of the file on the arweave permaweb is given by https://arweave.net/tx.id */
-        res.json({message: `The file upload costed ${price} AR, you have ${balance} AR remaining.`, id: id});
+        res.json({message: `The file upload costed ${price} AR. You have ${balance} AR remaining.`, id: id});
     }
 
 })
