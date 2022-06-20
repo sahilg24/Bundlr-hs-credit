872d09087b6f41de4acb354bce16f9edb0766bf5
Duy Tran <60637571+deantran98@users.noreply.github.com>
2022-06-20 16:32:45 -0400

    finished implementation of the submit form

--- client/src/App.js
+++ client/src/App.js
@@ -10,28 +10,42 @@ function App() {
   const [file, setFile] = useState();
   const [link, setLink] = useState();
   const [message, setMessage] = useState();
+  const [values, setValues] = useState({ firstName: "", lastName: "", });
+  const [uploaded, setUploaded] = useState(false);
+  const [valid, setValid] = useState(false);
 
-    async function upload(e) {
-      e.preventDefault();
+  async function upload(e) {
+    e.preventDefault();
+    if (values.firstName && values.lastName && file) {
+      setValid(true);
       setMessage("Please wait while the file is being submitted");
-      const startingTime = new Date()
-  
-      const formData = new FormData();
-      formData.append('uploadedFile', file)
-      const {data} = await api.post(formData);
-  
-      setMessage(data.message);
-      if (data?.id) {
-        setLink(`https://arweave.net/${data.id}`)
-      }
-  
-      const endingTime = new Date()
-      
-      console.log(`The upload/request took ${Math.abs(endingTime - startingTime) / 1000} seconds.`)
+    }
+    setUploaded(true);
+    const startingTime = new Date()
 
+    const formData = new FormData();
+    formData.append('uploadedFile', file)
+    const { data } = await api.post(formData);
+
+    setMessage(data.message);
+    setUploaded(true);
+    if (data?.id) {
+      setLink(`https://arweave.net/${data.id}`)
     }
 
-  
+    const endingTime = new Date()
+
+    console.log(`The upload/request took ${Math.abs(endingTime - startingTime) / 1000} seconds.`)
+
+  }
+
+  const handleFirstNameInput = (e) => {
+    setValues({ ...values, firstName: e.target.value })
+  }
+
+  const handleLastNameInput = (e) => {
+    setValues({ ...values, lastName: e.target.value })
+  }
 
   const onFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -50,9 +64,30 @@ function App() {
   return (
     <div className="App">
       <Bounce top><div className="header">Arweave Storage Using Bundlr Demo</div></Bounce>
-      <div className="bg-container">
+      <div className="form-container">
         <Flip top>
-          <form className="upload-form" onSubmit={upload} encType="multipart/form-data" >
+          <form class="upload-form" onSubmit={upload} encType="multipart/form-data">
+
+            {uploaded && valid ? <div class="success-message">Thank you for uploading a file!</div> : null}
+            <input
+              onChange={handleFirstNameInput}
+              id="first-name"
+              class="form-field"
+              type="text"
+              placeholder="Enter Your First Name"
+              name="firstName"
+            />
+            {uploaded && !values.firstName ? <span id="first-name-error" class="error">Please enter your first name</span> : null}
+            <input
+              onChange={handleLastNameInput}
+              id="last-name"
+              class="form-field"
+              type="text"
+              placeholder="Enter Your Last Name"
+              name="lastName"
+            />
+            {uploaded && !values.lastName ? <span id="last-name-error" class="error">Please enter your last name</span> : null}
+
             <div class="file-input">
               <input type="file" id="file" class="choose-file" onChange={onFileChange} />
               <label for="file">
@@ -60,6 +95,7 @@ function App() {
                 <p class="file-name"></p>
               </label>
             </div>
+            {uploaded && !file ? <span id="file-choose-error" class="error-file">Please choose a file</span> : null}
             <button type="submit" className="submit"><span>Submit</span></button>
           </form>
         </Flip>
@@ -77,8 +113,10 @@ function App() {
             {message}
           </p>
         }
+
       </div>
     </div>
+
   );
 
 
